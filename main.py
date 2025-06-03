c607e9102a6346649fc48576ad0b7c62d561eb12
unknown <zrtulman@gmail.com>
2025-06-02 17:18:19 -0700

    updated TODO list in main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from buddy import Buddy
 
-# TODO: get build OS-agnostic for dev/prod release split
+# TODO: modify build to be OS-agnostic for dev/prod release split (testing on unix platforms)
+#       SUB-TODO: create config-dev.py and add it to .gitignore.
+#                 config.py will be on the main repo for OS-agnosticism, config-dev.py lives in dev for windows only
 
 # TODO: add "transcription" mode where buddy just takes all the text it recognizes until it hears a key word then spits out the end result
 
 # TODO: "save messages", "how many?", save w/e number is given
-
-# TODO: Find a way to use new voices (I installed the natural Jenny voice, try it out)
-#   https://github.com/gexgd0419/NaturalVoiceSAPIAdapter can use this for Jenny voice, check if it's good first
 
 # TODO: Implement search.
 #   When phrase "search" is detected in message, call ChatGPT API for real search function
